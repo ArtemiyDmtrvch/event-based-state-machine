c41b5836f227cf71106c1259a454d43a132c04aa
ArtemiyDmtrvch <artemiy.dmtrvch@gmail.com>
2019-04-03 11:14:30 +0300

    no redundant demonstration classes

--- src/main/java/ru/impression/flow_architecture/Ext.kt
+++ src/main/java/ru/impression/flow_architecture/Ext.kt
@@ -1,4 +1,5 @@
 package ru.impression.flow_architecture
 
 @PublishedApi
-internal val Class<*>.notNullName get() = canonicalName ?: throw IllegalArgumentException("Dont use anonymous classes")
+internal val Class<*>.notNullName
+    get() = canonicalName ?: throw IllegalArgumentException("Dont use anonymous classes")
